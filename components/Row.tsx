ef01d48fc496e9c7f48d8e7c518cc789b415ca6e
vladcheck <valdemarcheck@yandex.ru>
2025-11-27 17:58:57 +0300

    feat: describe Row with JSDoc

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,19 @@
 import { ComponentProps, ReactNode } from "react";
 
+/**
+ * @function Row
+ * @description a container component that has `display: flex` on it, making it act as a 'row'
+ * @param {ComponentProps<"div">} props - props of a `<div>` tag
+ * @returns `ReactNode`
+ * @example
+ * 
+ * <Row>
+ *  <div class="employee-card">...</div>
+ *  <div class="employee-card">...</div>
+ *  <div class="employee-card">...</div>
+ * </Row>
+ * 
+*/
 export default function Row(props: ComponentProps<"div">): ReactNode {
   return (
     <div className="row" {...props}>
